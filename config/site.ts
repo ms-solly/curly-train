cd1465c6209df0865def58db32a61354fc7512e1
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-08-02 09:19:33 +0530

    ..

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "DOTA 2",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
