fc166f162199b23015bd1af95007fc7bb072ca09
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-08-02 21:26:34 +0530

    Delete LiveMatches.tsx

--- app/LiveMatches.tsx
+++
@@ -1,9 +0,0 @@
-import LiveMatches from "@/components/LiveMatches";
-
-const LiveMatchesPage = () => (
-  <section className="container">
-    <LiveMatches />
-  </section>
-);
-
-export default LiveMatchesPage;
