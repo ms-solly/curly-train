139718cd0ae1e63c8b6b9cb969b3371178e01007
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-08-08 00:26:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { buttonVariants } from "@/components/ui/button"
 import Image from "next/image"
-import bgImage from "/curly-train/public/bg.png"
+import bgImage from "public/bg.png"
 
 export default function IndexPage() {
   return (
