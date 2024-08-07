45bea472ece7876844d9d7fccb44293ca1c12f34
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-08-07 22:50:44 +0530

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,18 +9,17 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "underline-offset-4 hover:underline text-primary",
+        default: "bg-[#E4003A] text-white hover:bg-[#C30035]",
+        destructive: "bg-[#E4003A] text-white hover:bg-[#C30035]",
+        outline: "border-[#E4003A] text-[#E4003A] hover:bg-[#E4003A]/10",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-[#E4003A]/10 hover:text-[#E4003A]",
+        link: "underline-offset-4 hover:underline text-[#E4003A]",
+        outlineOnly: "border border-[#E4003A] bg-[#fff] text-black hover:bg-[#E4003A] hover:text-white",
       },
+      
       size: {
-        default: "h-10 py-2 px-4",
+        default: "h-8 py-2 px-4",
         sm: "h-9 px-3 rounded-md",
         lg: "h-11 px-8 rounded-md",
         icon: "h-10 w-10",
