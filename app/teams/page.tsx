d05e2552d2579db09b2eb56f24571359120b8cd9
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-08-09 00:20:07 +0530

    teams

---
+++ app/teams/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+import TeamsCard from '@/components/teams/TeamsCard';
+import React from 'react';
+
+const Teamspg = () => {
+    return (
+        <div>
+            <Header/>
+            <TeamsCard/>
+        </div>
+    );
+};
+
+export default Teamspg;
