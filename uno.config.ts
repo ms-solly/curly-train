60a8162b55a71110a8860580531110c6a4b8975f
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-08-13 02:34:47 +0530

    Add files via upload

---
+++ uno.config.ts
@@ -0,0 +1,15 @@
+// uno.config.ts
+import {
+    defineConfig,
+    presetAttributify,
+    presetIcons,
+    presetUno,
+    presetWebFonts
+  } from 'unocss'
+  
+  export default defineConfig({
+    presets: [
+      presetUno(),
+      // ...
+    ],
+  })
