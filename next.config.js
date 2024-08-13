a3f45066281c9ed78074d47d33e90cabc28e94fd
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-08-13 18:48:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: async (config) => {
-    const UnoCSS = (await import('@unocss/webpack')).default;
-    config.plugins.push(UnoCSS());
-    return config;
-  },
   images: {
     remotePatterns: [
       {
