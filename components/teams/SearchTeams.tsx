c5c4595ec94c9307d43cf7386c182354777a98c3
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-08-23 17:08:16 +0530

    Update SearchTeams.tsx

--- components/teams/SearchTeams.tsx
+++ components/teams/SearchTeams.tsx
@@ -8,7 +8,7 @@ const SearhTeamsInp = () => {
     return (
         <div className='flex justify-center m-5 mb-10'>
             {/* <FaSearch className=' inline-block m-2 mt-3  justify-center'/> */}
-            <input type="input" list='heroes' name="search" id="search" placeholder='Search heroes' className=' border-box w-2/3 rounded border-2 border-solid  p-2 outline-none focus:border-green-500  focus:transition-all lg:w-1/3 '/>
+            <input type="input" list='heroes' name="search" id="search" placeholder='Search Teams' className=' border-box w-2/3 rounded border-2 border-solid  p-2 outline-none focus:border-green-500  focus:transition-all lg:w-1/3 '/>
             <datalist id='heroes'>
                 <option value="Gaimin Gladiators"></option>
                 <option value="WBG.XG"></option>
