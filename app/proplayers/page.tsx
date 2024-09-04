5d64853f3755ca218eb530b78c31e2a7af229e07
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-09-04 20:31:48 +0530

    Update page.tsx

--- app/proplayers/page.tsx
+++ app/proplayers/page.tsx
@@ -6,6 +6,7 @@ import Pagination from '@/components/Pagination';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 const getProPlayers = async () => 
     action('proPlayers', config.API_HOST, 'api/proplayers');
@@ -60,37 +61,37 @@ const ProPlayersPage: React.FC = () => {
                 <h1 className="text-4xl font-bold mb-4 text-center text-white-800 font-rubik scrollbar-track-slate-800">Pro Players</h1>
                 {proPlayers.length > 0 ? (
                     <div>
-                        <table className="min-w-full text-white rounded-lg overflow-hidden shadow-md backdrop-blur-md bg-white/5 p-4 border border-gray-200">
-                            <thead>
-                                <tr className="font-rubik bg-white/10 backdrop-blur-md text-sky-300">
-                                    <th className="px-3 py-2 text-left">Name</th>
-                                    <th className="px-3 py-2 text-left">Team</th>
-                                    <th className="px-3 py-2 text-left">Country</th>
-                                    <th className="px-3 py-2 text-left">Last Login</th>
-                                    <th className="px-3 py-2 text-left">Profile</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                        <Table className="rounded-lg shadow-md backdrop-blur-md bg-white/5 p-4 border border-gray-200">
+                            <TableHeader className='bg-black hover:bg-black'>
+                                <TableRow className="font-rubik bg-black hover:bg-black backdrop-blur-md text-green-300">
+                                    <TableHead className="px-3 py-2 text-left">Name</TableHead>
+                                    <TableHead className="px-3 py-2 text-left">Team</TableHead>
+                                    <TableHead className="px-3 py-2 text-left">Country</TableHead>
+                                    <TableHead className="px-3 py-2 text-left">Last Login</TableHead>
+                                    <TableHead className="px-3 py-2 text-left">Profile</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
                                 {currentProPlayers.map((player: any) => (
-                                    <tr key={player.steamid} className="border-b border-gray-700 hover:bg-sky-200 hover:text-gray-800">
-                                        <td className="px-4 py-2 flex items-center hover:text-white">
-                                            <Link href={`/proplayers/${player.steamid}`} className="flex items-center text-sky-400 font-bold font-rubik hover:text-white">
+                                    <TableRow key={player.steamid} className="border-b border-gray-700 hover:bg-green-200 hover:text-gray-800">
+                                        <TableCell className="px-4 py-2 flex items-center hover:text-white">
+                                            <Link href={`/proplayers/${player.steamid}`} className="flex items-center text-green-400 font-bold font-rubik hover:text-white">
                                                 <Image src={player.avatarfull} alt={player.personaname} width={48} height={48} className="w-12 h-12 object-cover rounded-full mr-2" />
                                                 {player.personaname}
                                             </Link>
-                                        </td>
-                                        <td className="px-4 py-2 font-rubik">{player.team_name}</td>
-                                        <td className="px-4 py-2 font-rubik">{player.loccountrycode || 'N/A'}</td>
-                                        <td className="px-4 py-2 font-rubik">{new Date(player.last_login).toLocaleDateString()}</td>
-                                        <td className="px-4 py-2 font-rubik">
-                                            <Button className="bg-sky-500 hover:bg-sky-700 rounded-md px-3 h-8 hover:text-white">
+                                        </TableCell>
+                                        <TableCell className="px-4 py-2 font-rubik">{player.team_name}</TableCell>
+                                        <TableCell className="px-4 py-2 font-rubik">{player.loccountrycode || 'N/A'}</TableCell>
+                                        <TableCell className="px-4 py-2 font-rubik">{new Date(player.last_login).toLocaleDateString()}</TableCell>
+                                        <TableCell className="px-4 py-2 font-rubik">
+                                            <Button className="bg-green-500 hover:bg-green-700 rounded-md px-3 h-8 hover:text-white">
                                                 <Link href={`/proplayers/${player.steamid}`}>View Profile</Link>
                                             </Button>
-                                        </td>
-                                    </tr>
+                                        </TableCell>
+                                    </TableRow>
                                 ))}
-                            </tbody>
-                        </table>
+                            </TableBody>
+                        </Table>
                         <Pagination
                             itemsPerPage={itemsPerPage}
                             totalItems={totalItems}
