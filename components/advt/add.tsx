e608d4f9805dba172aab7ee1397afb928196e981
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-12-12 03:17:06 +0530

    Update add.tsx

--- components/advt/add.tsx
+++ components/advt/add.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Button } from "../ui/button";
+import { Button } from "../ui/button";  
 
 export default function Advertizemen() {
   const [isVisible, setIsVisible] = React.useState(true);
@@ -12,7 +12,7 @@ export default function Advertizemen() {
 
   return (
     isVisible && (
-      <div className=" ad-section bg-gradient-to-r from-[#E4003A] to-[#FF5959] text-white h-14 w-full flex items-center justify-between px-6 border-b-2 border-gray-950 shadow-md">
+      <div className="ad-section bg-gradient-to-r from-[#E4003A] to-[#FF5959] text-white h-14 w-full flex items-center justify-between px-6 border-b-2 border-gray-950 shadow-md">
         <div className="flex items-center space-x-4">
           <div className="text-lg font-semibold">
             <span>Sponsored Ad</span>
@@ -23,6 +23,7 @@ export default function Advertizemen() {
           onClick={closeAdSection}
           className="text-white text-2xl font-bold p-1 hover:bg-opacity-80 rounded-full focus:outline-none bg-transparent hover:bg-transparent"
           aria-label="Close Advertisement"
+          title="Close Advertisement"
         >
           &times;
         </Button>
